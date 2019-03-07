3db16e857d13ceb107551648fc76b18f812fcbcc
hasakei <446109730@qq.com>
2019-03-07 10:01:57 +0800

    springboot

---
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("hello")
+    public String hello(){
+
+        return "helloWorld";
+    }
+
+    @RequestMapping("hello1")
+    public String hello1(){
+
+        return "helloWorld";
+    }
+}
---
+++ src/test/java/com/example/springboot/Test.java
@@ -0,0 +1,9 @@
+package com.example.springboot;
+
+public class Test {
+    public static void main(String[] args) {
+        String replace = "123".replace("1", "2");
+        System.out.println(replace);
+        System.out.println("123");
+    }
+}
