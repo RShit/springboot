395d61edc43ab19409eb9f2232290e9e6d7f0a87
hasakei <446109730@qq.com>
2019-05-21 12:13:53 +0800

    删除不用文件

--- src/test/java/com/example/springboot/Test.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot;
-
-public class Test {
-    public static void main(String[] args) {
-        String replace = "123".replace("1", "2");
-        System.out.println(replace);
-        System.out.println("123");
-    }
-}
